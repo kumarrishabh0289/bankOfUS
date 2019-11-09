5a46bbf738a36514918de5282e0e1e7e34fec771
Divisha Bera <divisha.bera@sjsu.edu>
2019-11-09 01:26:21 -0800

    Signed-off-by: Divisha Bera <divisha.bera@sjsu.edu>

--- backend/server.js
+++
@@ -1,6 +0,0 @@
-const http = require('http');
-const app = require('./app');
-const port = process.env.PORT || 3001;
-const server = http.createServer(app);
-server.listen(port);
-console.log('Starting server at:'+port);
