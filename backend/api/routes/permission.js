cdc56306bc16dabd379566b624028e737a5d7026
Divisha Bera <divisha.bera@sjsu.edu>
2019-12-06 02:01:39 -0800

    Signed-off-by: Divisha Bera <divisha.bera@sjsu.edu>

--- backend/api/routes/permission.js
+++ backend/api/routes/permission.js
@@ -4,25 +4,7 @@ const mongoose = require('mongoose');
 const Permission = require('../models/permission');
 
 
-    const course_id = req.query.course;
-    Permission.find({ course_id: course_id })
-        .exec()
-        .then(doc => {
-            console.log("From database", doc);
-            if (doc) {
-                res.status(200).json(doc);
-            }
-            else {
-                res.status(404).json({ message: "not a valid ID" });
-            }
-
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({ error: err });
-        })
-
-});
+ 
 
 router.post('/', (req, res, next) => {
     
