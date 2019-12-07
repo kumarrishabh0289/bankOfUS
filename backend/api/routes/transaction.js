8d0c16d20539ee2f3caf69372f47421c9c60e311
arpitsharrma <arpit.sharma@sjsu.edu>
2019-12-07 01:13:19 -0800

    removed dead code

--- backend/api/routes/transaction.js
+++ backend/api/routes/transaction.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const Transaction = require('../models/transaction');
 const User = require('../models/user');
-var jwt = require('jsonwebtoken');
 
 
 router.get('/getall', (req, res, next) => {
