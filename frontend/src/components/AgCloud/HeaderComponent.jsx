11bd93aa52dbb7aba8ccbd12a39b66009d71db28
ayushi1115 <ayushi.singh@sjsu.edu>
2019-12-07 02:23:07 -0800

    Transaction fix

--- frontend/src/components/AgCloud/HeaderComponent.jsx
+++ frontend/src/components/AgCloud/HeaderComponent.jsx
@@ -13,7 +13,7 @@ class HeaderComponent extends Component {
                 <nav className="navbar navbar-expand-md bg-light navbar-light ">
                     <div><img src="logo.png" height="40" width="55"></img> <a href="/" className="navbar-brand">Bank of USA</a></div>
                     <ul className="navbar-nav">
-                        {isUserLoggedIn && <li><Link className="nav-link" to="/welcome/AGMachineCloud">Home</Link></li>}
+                        {isUserLoggedIn && <li><Link className="nav-link" to="/welcome/bankofusa">Home</Link></li>}
                         
                     </ul>
                     <ul className="navbar-nav navbar-collapse justify-content-end">
